93b03cc1b5bd2f60fd483b7e621e7a8e691ba13d
SnokieSnoks <snokiesnoks@gmail.com>
2025-12-09 22:53:52 -0300

    Delete src directory

--- src/contexts/AudioPlayerContext.1.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import type { AudioPlayerContextType } from '../typings/typings';
-
-export const AudioPlayerContext = React.createContext<
-  AudioPlayerContextType | undefined
->(undefined);
